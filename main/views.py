8ef0e3d71d30d0298c83be067fe33231754c54db
ochirovaur2 <ochirovaur2@mail.ru>
2021-08-26 17:51:30 +0300

    commit

--- main/views.py
+++ main/views.py
@@ -8,9 +8,20 @@ from .services.handle_get_followers import handle_get_followers
 import json
 import datetime
 
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
 @csrf_exempt
 @api_view(['GET'])
 def get_current_time(request):
+	print(get_client_ip(request))
 	
 	print(datetime.datetime.now())
 	return Response( {"current_time": datetime.datetime.now()} , status=status.HTTP_200_OK)
